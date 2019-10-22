e6ab0c31e6f4aaf45dcae8f3b44e5bac084276d1
Jörg Forstner <joergforstner@hotmail.com>
2019-10-22 20:42:15 +0200

    Projektdateien hinzufügen.

---
+++ ThereIsNoSpoonCodingAme/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThereIsNoSpoonCodingAme
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+    }
+  }
+}
